32459d14319f3cbc30f0a6d2db668559c56b106d
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-22 15:47:52 -0400

    Projeto criado

---
+++ src/test/java/com/cesarbassani/apprest/ApprestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cesarbassani.apprest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApprestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
