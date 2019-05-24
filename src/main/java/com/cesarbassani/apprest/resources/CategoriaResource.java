7b3f7383eb40754ab1e8bd3fcb5068f0e6862c4d
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-24 16:18:16 -0400

    Listando todas as categorias com DTO

--- src/main/java/com/cesarbassani/apprest/resources/CategoriaResource.java
+++ src/main/java/com/cesarbassani/apprest/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.cesarbassani.apprest.resources;
 
 import com.cesarbassani.apprest.domain.Categoria;
+import com.cesarbassani.apprest.dto.CategoriaDTO;
 import com.cesarbassani.apprest.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -43,6 +46,14 @@ public class CategoriaResource {
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
         categoriaService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = categoriaService.findAll();
+
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
--- src/main/java/com/cesarbassani/apprest/services/CategoriaService.java
+++ src/main/java/com/cesarbassani/apprest/services/CategoriaService.java
@@ -6,8 +6,10 @@ import com.cesarbassani.apprest.services.exceptions.DataIntegrityException;
 import com.cesarbassani.apprest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,9 @@ public class CategoriaService {
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível uma categoria que possui produtos!");
         }
+    }
+
+    public List<Categoria> findAll() {
+        return categoriaRepository.findAll();
     }
 }
