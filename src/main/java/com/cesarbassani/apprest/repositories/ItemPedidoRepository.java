e760efc13a4a4c75b1fde963e6a35e84e2100825
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-24 11:49:46 -0400

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/cesarbassani/apprest/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.cesarbassani.apprest.repositories;
+
+import com.cesarbassani.apprest.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+}
