b62a9b9637da89cc6c57257e4c8b256cca1724b8
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-05-24 02:17:10 -0400

    Pedido, Pagamento e EstadoPagamento

---
+++ src/main/java/com/cesarbassani/apprest/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.cesarbassani.apprest.repositories;
+
+import com.cesarbassani.apprest.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
---
+++ src/main/java/com/cesarbassani/apprest/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.cesarbassani.apprest.repositories;
+
+import com.cesarbassani.apprest.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
